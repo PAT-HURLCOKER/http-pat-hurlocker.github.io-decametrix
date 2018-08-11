8afa81a7b97f248fe333cb9fafce23d06eba9bac
Marcus C Manahan <c.manahanmarcus@yahoo.com>
2018-08-11 13:29:01 -0700

    Group project commit

---
+++ config.py
@@ -0,0 +1,11 @@
+#Consumer API keys
+consumer_key = "XRrY6w3mtqQGeVvBHzZg9YhxM" 
+
+consumer_secret = "OWwkseJDxQR72OY4J3tWVGuSBwFsyxAd5W8KTZhYKtaKHJW25M" 
+
+
+#Access token & access token secret
+access_token = "4254475753-4wMzTTtBeQ4fx6JD8qzBdT2DzvB7R7xv9009U0I" 
+
+access_token_secret = "g3faWALG8s6JfV2mjjkEMNpyos9q8v2VGulSVkdqVPCz0" 
+
